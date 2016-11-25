956a8f644be5c5a9685c91574c639a010224c3f5
baige <438759512@qq.com>
2016-11-25 16:35:44 +0800

    白欣雨v0.1

--- app/scripts/controllers/baixinyu.js
+++ app/scripts/controllers/baixinyu.js
@@ -1,4 +1,16 @@
 angular.module("bugcenterApp").controller("Bx",["$scope","$http","$interval",function($scope,$http,$interval){
-	
+	$scope.labels = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
+    $scope.data = [300, 500, 100];
 }])
 	
+
+
+
+
+
+
+
+
+  
+  
+
