f2d23d5ecc0a7e290edef3edd246b31f1658539e
shiyifei <1148655376@qq.com>
2016-11-25 16:32:00 +0800

    石奕飞v0.1

--- app/scripts/controllers/shiyifei.js
+++ app/scripts/controllers/shiyifei.js
@@ -1,4 +1,20 @@
-angular.module("bugcenterApp").controller("Sy",["$scope","$http","$interval",function($scope,$http,$interval){
-	
+angular.module("bugcenterApp").controller("Sy",["$scope","$http","$interval","$timeout",function($scope,$http,$interval,$timeout){
+	$scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+	$scope.series = ['Series A', 'Series B'];
+	$scope.data = [
+	    [65, 59, 80, 81, 56, 55, 40],
+	    [28, 48, 40, 19, 86, 27, 90]
+	];
+	$scope.onClick = function (points, evt) {
+	    console.log(points, evt);
+	};
+
+	  // Simulate async data update
+	$timeout(function () {
+	    $scope.data = [
+	      [28, 48, 40, 19, 86, 27, 90],
+	      [65, 59, 80, 81, 56, 55, 40]
+	    ];
+	}, 3000);
 }])
 	
