7e116cd725a629ed2d729e5ba84ddd4f4b3067b7
lining <1046674804@qq.com>
2016-11-26 15:25:53 +0800

    李宁v0.1

--- app/scripts/controllers/Lnlist.js
+++ app/scripts/controllers/Lnlist.js
@@ -1,6 +1,18 @@
 
-angular.module("bugcenterApp").controller("Ln",["$scope","$http","$interval","$timeout",function($scope,$http,$interval,$timeout){
+angular.module("bugcenterApp").controller("Ln",["$rootScope","$scope","$http","$interval","$timeout",function($rootScope,$scope,$http,$interval,$timeout){
+	$scope.dataname=["Ui设计","前端","后台"]
+	$scope.frequency=["偶尔","经常"]
+	$scope.importance =["重要","中等","一般"]
+	$scope.jinc="偶尔"
+	$scope.imp = "重要"
+	$scope.Users = "lnn"
 	$scope.Jue=["Ui设计","前端","后台"]
+	$scope.Fabu="Fabu"
+	$scope.userName = []
+	//默认已指派
+	$scope.status =0
+	console.log($scope.u)
+	//图表信息
 	$scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
 	$scope.series = ['Series A', 'Series B'];
 	$scope.data = [
@@ -18,12 +30,18 @@ angular.module("bugcenterApp").controller("Ln",["$scope","$http","$interval","$t
 	      [65, 59, 80, 81, 56, 55, 40]
 	    ];
 	}, 3000);
+	//获取所有用户
 	$http({
 		url:"http://www.bugcenter.com.cn:1511/users",
 		method:"get",
 	}).success(function(e){
-		$scope.Usern=e
-		$scope.Username = e.username
+		//过滤测试人员获取所有其他用户
+		for(var i =0;i<e.length;i++){
+			if(e[i].charactor!=3){
+				$scope.userName.push(e[i].username)
+			}
+		}
+		
 		$http({
 			url:"http://www.bugcenter.com.cn:1511/item",
 			method:"get",
@@ -51,8 +69,67 @@ angular.module("bugcenterApp").controller("Ln",["$scope","$http","$interval","$t
 			$scope.UiL = $scope.Ui.length
 			$scope.QdL = $scope.Qd.length
 			$scope.HtL = $scope.Ht.length
-			console.log($scope.QdL)
+			// console.log($scope.QdL)
 		})
 	})
+	$scope.FabuLn = function(){
+		$scope.Fabu="Fabu1"
+	}
+	//声明空对象
+	$scope.uesrifm = {}
+	//判断bug分类（ui设计、前端、后台）
+	$scope.fn = function(index){
+		$scope.uesrifm.classify=index
+	}
+	//点击发布判断bug频率（偶尔、经常）
+	$scope.Submit = function(){
+		if($scope.jinc=="偶尔"){
+			$scope.uesrifm.frequency=0
+		}else if($scope.jinc=="经常"){
+			$scope.uesrifm.frequency=1
+		}
+		//判断bug重要性（重要、中等、一般）
+		if($scope.imp=="重要"){
+			$scope.uesrifm.importance =0
+		}else if($scope.imp=="中等"){
+			$scope.uesrifm.importance =1
+		}else if($scope.imp=="一般"){
+			$scope.uesrifm.importance =2
+		}
+		//存bug简介到$scope.uesrifm.summary
+		$scope.uesrifm.summary=$scope.Textarea
+		//存bug详情到$scope.uesrifm.description
+		$scope.uesrifm.description=$scope.text1
+		//获取当前时间 
+		var mydate = new Date()
+		var Year = mydate.getFullYear()
+		var month = mydate.getMonth()+1
+		var day = mydate.getDate()
+		var Hous = mydate.getHours()
+		var Min = mydate.getMinutes()
+		var Se = mydate.getSeconds()
+		var Mydate = Year+'-'+month+'-'+day+' '+Hous+':'+Min+':'+Se
+		//存当前时间到$scope.uesrifm.date
+		$scope.uesrifm.date =Mydate
+		//获取存bug指向（即bug发布给谁） 
+		$scope.uesrifm.to =$scope.Users
+		//默认存bug已指派
+		$scope.uesrifm.status=$scope.status 
+		//存bug发布人
+		$scope.uesrifm.from =sessionStorage.getItem("username")
+		//执行$http,发布bug
+		$http({
+			url:"http://www.bugcenter.com.cn:1511/item",
+			method:"post",
+			data:$scope.uesrifm
+		}).success(function(e){
+			$scope.Fabu="Fabu"
+			console.log(e)
+		})
+	}
+
+	$scope.back = function(){
+		$scope.Fabu="Fabu"
+	}
 }])
 	
