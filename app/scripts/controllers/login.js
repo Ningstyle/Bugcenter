3e3cd354c77e275de30f4fd5aa9fbd06c9e73328
lining <1046674804@qq.com>
2016-11-26 19:32:49 +0800

    李宁v0.4

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -1,39 +1,48 @@
-angular.module("bugcenterApp").controller("Login",["$rootScope","$scope","$http","$state","$interval",function($rootScope,$scope,$http,$state,$interval){
+angular.module("bugcenterApp").controller("Login",["$rootScope","$scope","$http","$state","$interval","$timeout",function($rootScope,$scope,$http,$state,$interval,$timeout){
 	$scope.class=$scope.class1='form-group has-success'
 	$scope.check=false
 	$scope.login = function(){
-			$http({
-				url:"http://www.bugcenter.com.cn:1511/users/login",
-				method:"post",
-				data:$scope.updata
-			}).success(function(e){
-				$scope.uid=e.uid
-				$http({
-					url:"http://www.bugcenter.com.cn:1511/users/"+$scope.uid,
-					method:"get",
-				}).success(function(e){
-					console.log(e)
-					if(e.charactor==0){
-						$rootScope.UserName = {}
-						$rootScope.UserName.username=e.username
-						$state.go("/Sy")
-					}
-					if(e.charactor==1){
-						$state.go("/Lx")
-						$rootScope.UserName = {}
-						$rootScope.UserName.username=e.username
-					}
-					if(e.charactor==2){
-						$state.go("/Bx")
-						$rootScope.UserName = {}
-						$rootScope.UserName.username=e.username
-					}
-					if(e.charactor==3){
-						$state.go("/Ln")
-						sessionStorage.setItem("username",e.username)
-					}
-				})
-			})
+		if($scope.updata&&$scope.updata.username==null){
+			$scope.Lntext="请正确输入账号信息!"
+			$scope.Motai="Motai1"
+			$timeout(function(){
+				$scope.Motai="Motai"
+			},2000)
+				// $http({
+				// 	url:"http://www.bugcenter.com.cn:1511/users/login",
+				// 	method:"post",
+				// 	data:$scope.updata
+				// }).success(function(e){
+				// 	$scope.uid=e.uid
+				// 	$http({
+				// 		url:"http://www.bugcenter.com.cn:1511/users/"+$scope.uid,
+				// 		method:"get",
+				// 	}).success(function(e){
+				// 		console.log(e)
+				// 		if(e.charactor==0){
+				// 			$rootScope.UserName = {}
+				// 			$rootScope.UserName.username=e.username
+				// 			$state.go("/Sy")
+				// 		}
+				// 		if(e.charactor==1){
+				// 			$state.go("/Lx")
+				// 			$rootScope.UserName = {}
+				// 			$rootScope.UserName.username=e.username
+				// 		}
+				// 		if(e.charactor==2){
+				// 			$state.go("/Bx")
+				// 			$rootScope.UserName = {}
+				// 			$rootScope.UserName.username=e.username
+				// 		}
+				// 		if(e.charactor==3){
+				// 			$state.go("/Ln")
+				// 			sessionStorage.setItem("username",e.username)
+				// 		}
+				// 	})
+				// })
+			}else{
+				alert(2)
+			}
 		}
 }])
 	
---
+++ app/scripts/shiyifei2.js
@@ -0,0 +1,8 @@
+   angular.module("syf",[]).directive("shoufenqing",function(){
+      return{
+        restrict:'ECMA',
+        template:'<div class="panel panel-default" ng-repeat="x in dataOne"><div class="panel-heading" role="tab" id="heading{{$index+1}}"><h4 class="panel-title"><a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapse{{$index+1}}" aria-expanded="false" aria-controls="collapse{{$index+1}}">{{x.summary}}</a></h4></div><div id="collapse{{$index+1}}" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading{{$index+1}}"><div class="panel-body">{{x.description}}</div></div></div>',
+        replace:true,
+        scope:{dataOne:"=data"},
+      }
+   })
